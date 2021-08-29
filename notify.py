c3c619f7c77ae2accc11e0c38f0f72e874645300
asajjad38 <asajjad94@gmail.com>
2021-08-29 15:16:31 -0400

    Initial commit

---
+++ notify.py
@@ -0,0 +1,22 @@
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+from scraper import requestGME
+from scraper import get_proxies
+
+load_dotenv()
+
+account_sid = os.getenv('ACCOUNT')
+auth_token = os.getenv('TOKEN')
+
+client = Client(account_sid, auth_token)
+
+proxies = get_proxies()
+availability = requestGME(proxies)
+
+if availability == 'Available':
+    message = client.messages.create (
+        to = os.getenv('RECIPIENT'),
+        from_= os.getenv('SENDER'),
+        body = "The playstation 5 is in stock now on Gamestop.com"
+    )
---
+++ scraper.py
@@ -0,0 +1,48 @@
+import requests
+import json
+from random import randrange
+from fake_useragent import UserAgent
+from lxml.html import fromstring
+from bs4 import BeautifulSoup
+
+user_agent = UserAgent()
+
+URL = "https://www.gamestop.com/consoles-hardware/playstation-5/consoles/products/sony-playstation-5-console/11108140.html?condition=New"
+
+headers = {
+    'User-Agent' : user_agent.random
+}
+
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = []
+    for i in parser.xpath('//tbody/tr')[:1000]:
+        if i.xpath('.//td[7][contains(text(),"yes")]') and i.xpath('.//td[4][contains(text(),"United States")]'):
+            #Grabbing IP and corresponding PORT
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.append(proxy)
+    return proxies
+
+def parseHTML(response):
+    soup = BeautifulSoup(response.content, "html.parser")
+    results = soup.find(id="add-to-cart")
+    value = str(results["data-gtmdata"])
+    valueJSON = json.loads(value)
+    return valueJSON['productInfo']['availability']
+
+def requestGME(proxies):
+    random = randrange(len(proxies))
+    proxy = proxies[random]
+    print(proxy)
+    print(headers['User-Agent'])
+    try:
+        response = requests.get(URL, headers=headers, proxies={'http': 'http://' + proxy, 'https': 'https://' + proxy}, timeout=10)
+        if response.status_code == 200:
+            availability = parseHTML(response)
+            return availability
+    except:
+        print('Connection Error with request')
+        headers['User-Agent'] = user_agent.random
+        return requestGME(proxies)
